e367b63598b0ce1f1a5ef0afe947503d1d9b355c
Sashmorra <lebedev2008.a@gmail.com>
2025-02-09 13:27:10 +0300

    dev

--- src/task/task-date-graph/task-date-graph.module.ts
+++ src/task/task-date-graph/task-date-graph.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TaskDateGraphController } from './task-date-graph.controller';
 import { TaskDateGraphService } from './task-date-graph.service';
 import { PrismaService } from '@/prisma.service';
+import { TaskDateGraphRepository } from '@/task/task-date-graph/db/repository/task-date-graph.repository'
 
 @Module({
   controllers: [TaskDateGraphController],
-  providers: [TaskDateGraphService, PrismaService]
+  providers: [TaskDateGraphService, PrismaService, TaskDateGraphRepository]
 })
 export class TaskDateGraphModule {}
